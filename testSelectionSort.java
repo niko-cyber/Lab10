fe1cc7a153662beaef8914091d6c0c70d38b33f3
Doemniko Kodra <domeniko@umich.edu>
2021-12-09 16:44:25 -0500

    added test dupicate and mixed

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -43,11 +43,45 @@ testDuplicates();
 
     public void testMixed(){
 
-        /** Test data contains with both positive, negative and zeros **/
-    }
+     /** Test data contains with both positive, negative and zeros **/
+   int[] arr = new int[5];
+     arr[0] = -8;
+     arr[1] = 9;
+     arr[2] = -7;
+     arr[3] = 10;
+     arr[4] = 2;
 
-    public void testDuplicates(){
+     int[] Sortedarr = new int[5];
+     Sortedarr[0] = -8;
+     Sortedarr[1] = -7;
+     Sortedarr[2] = 2;
+     Sortedarr[3] = 9;
+     Sortedarr[4] = 10;
+     SelectionSort temp1=new SelectionSort();
+     int sortArray[]=temp1.basicSelectionSort(arr);
 
-        /** Test data contains duplicates **/
-    }
+     assertArrayEquals("Test selection sort for mixed value",Sortedarr,sortArray);
+ }
+ public void testDuplicates(){
+
+     /** Test data contains duplicates **/
+   int[] arr = new int[5];
+     arr[0] = 8;
+     arr[1] = 9;
+     arr[2] = 8;
+     arr[3] = 10;
+     arr[4] = 2;
+
+     int[] Sortedarr = new int[5];
+     Sortedarr[0] = 2;
+     Sortedarr[1] = 8;
+     Sortedarr[2] = 8;
+     Sortedarr[3] = 9;
+     Sortedarr[4] = 10;
+     SelectionSort temp1=new SelectionSort();
+     int sortArray[]=temp1.basicSelectionSort(arr);
+
+     assertArrayEquals("Test selection sort for duplicates value",Sortedarr,sortArray);
+
+ }
 }
